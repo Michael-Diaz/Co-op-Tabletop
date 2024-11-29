f52b88e2aacd9f3216cc64def96eaef23213d480
Michael Diaz <michaeldiaz.compsci@gmail.com>
2024-11-29 17:27:46 -0500

    Update to Animation Controller
    - Integrated new animations
    - Restructured animation controller to utilize sub-state machines
    - Re-blended animations

--- Assets/Scipts/Michael/TEST_ProxyCharacter_Anim.cs
+++ Assets/Scipts/Michael/TEST_ProxyCharacter_Anim.cs
@@ -30,7 +30,8 @@ public class TEST_ProxyCharacter_Anim : MonoBehaviour
 
     private int heldItem_totalStates;
 
-    private bool isInPain;
+    private bool isDeathsDoor;
+    private bool isDead;
 
     // Start is called before the first frame update
     void Start()
@@ -41,8 +42,6 @@ public class TEST_ProxyCharacter_Anim : MonoBehaviour
         currentState_int = (int) currentState;
 
         heldItem_totalStates = Enum.GetNames(typeof(HeldItemState)).Length;
-
-        isInPain = false;
     }
 
     // Update is called once per frame
@@ -50,14 +49,20 @@ public class TEST_ProxyCharacter_Anim : MonoBehaviour
     {
         bool isStatechanged = false;
 
+        // Move up the list of held-item states
         if (Input.GetKeyDown(KeyCode.I))
         {
-            currentState_int = Mathf.Clamp(currentState_int - 1, 0, heldItem_totalStates - 1);
+            currentState_int = (currentState_int - 1) % heldItem_totalStates;
+            if (currentState_int < 0)
+                currentState_int += heldItem_totalStates;
+
             isStatechanged = true;
         }
+        // Move down the list of held-item states
         if (Input.GetKeyDown(KeyCode.K))
         {
-            currentState_int = Mathf.Clamp(currentState_int + 1, 0, heldItem_totalStates - 1);
+            currentState_int = (currentState_int + 1) % heldItem_totalStates;
+
             isStatechanged = true;
         }
 
@@ -69,15 +74,65 @@ public class TEST_ProxyCharacter_Anim : MonoBehaviour
             animator.SetTrigger("Switch State");
         }
 
-        if (Input.GetKeyDown(KeyCode.X))
-        {
-            isInPain = !isInPain;
-            animator.SetBool("IsHurt", isInPain);
-        }
-
+        // Have the player take physical damage
         if (Input.GetKeyDown(KeyCode.Z))
         {
+            Debug.Log("<color=#666600>Taking Physical Damage...</color>");
+            DamagePlayer(0);
+        }
+        // Have the player take psychic damage
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            Debug.Log("<color=#cc00cc>Taking Psychic Damage...</color>");
+            DamagePlayer(1);
+        }
+
+        // Heal the player for one state
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            Debug.Log("<color=#00ff00>Healing...</color>");
+            if (isDead)
+            {
+                isDead = false;
+                animator.SetBool("IsDead", false);
+            }
+            else if (isDeathsDoor)
+            {
+                isDeathsDoor = false;
+                animator.SetBool("IsHurt", false);
+            }
+        }
+
+        // Player attacks
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            Debug.Log("<color=#dd0000>Attacking...</color>");
             animator.SetTrigger("Attack");
         }
+
+        // Player casts a non-damaging spell
+        if (Input.GetKeyDown(KeyCode.E)) 
+        {
+            Debug.Log("<color=#00ffff>Casting...</color>");
+            animator.SetTrigger("Cast");
+        }
+    }
+
+    private void DamagePlayer(int damageType)
+    {
+        animator.SetInteger("Damage Type Enum", damageType);
+
+        if (isDeathsDoor && !isDead)
+        {
+            isDead = true;
+            animator.SetBool("IsDead", true);
+        }
+        else if (!isDeathsDoor)
+        {
+            isDeathsDoor = true;
+            animator.SetBool("IsHurt", true);
+        }
+
+        animator.SetTrigger("Take Damage");
     }
 }
